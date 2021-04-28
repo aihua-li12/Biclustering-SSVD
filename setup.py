fb0303dd1f70fdc014021b1abd09ff798a1cde06
aihua-li12 <69448318+aihua-li12@users.noreply.github.com>
2021-04-28 20:25:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,15 @@ setup(name='SSVD',
       author='Aihua Li, Yuxuan Chen',
       author_email='yc450@duke.edu',
       packages=['SSVD'],
+      url='https://github.com/YuxuanMonta/STA663-final-project-AY',
+      classifiers=[
+                  'Intended Audience :: Developers',
+                  'Topic :: Software Development :: Libraries :: Python Modules',
+                  'License :: OSI Approved :: MIT License',
+                  'Programming Language :: Python :: 3',
+                  ],
+      package_dir={"": "src"},
+      packages=setuptools.find_packages(where="src"),
+      python_requires=">=3.6",
       zip_safe=False)
-
-
 
