6e4b0be3d9d6d0c8feb83e1a0a59ccec3dacc4eb
YuxuanMonta <70059492+YuxuanMonta@users.noreply.github.com>
2021-04-28 13:50:04 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='SSVD',
+      version='0.1',
+      description='SSVD algorithm for biclustering',
+      author='Aihua Li, Yuxuan Chen',
+      author_email='yc450@duke.edu',
+      packages=['SSVD'],
+      zip_safe=False)
+
+
+
